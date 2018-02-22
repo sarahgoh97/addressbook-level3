13f5c2b58858f4c20440397a26f0b381275ac342
sarahgoh97 <sarahgohshining97@yahoo.com.sg>
2018-02-23 00:20:55 +0800

    Add header comments for interface

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -1,4 +1,11 @@
 package seedu.addressbook.data.person;
 
+
+/**
+ * A printable immutable interface for data of a Person in the addressbook.
+ * Implementations should guarantee: details are of String type to be printed.
+ */
 public interface Printable {
+
+    String getPrintableString();
 }
